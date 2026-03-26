16d73f1414669a692b78a2c5cc36aa6480f605ed
adith <krishnaadith23@gmail.com>
2026-03-26 10:29:57 +0530

    second commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .forms import StudentForm 
+
+def student_create(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = StudentForm()
+
+    return render(request, "student_form.html", {"form": form})
